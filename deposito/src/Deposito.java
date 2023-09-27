f252a0f3d6c8e5a08e5591c2671356fc7ec5e13f
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 19:14:26 -0300

    commit add.

--- deposito/src/Deposito.java
+++ deposito/src/Deposito.java
@@ -1,25 +1,28 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Deposito {
+public class Deposito {    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
 
-        while (scanner.hasNextDouble()) {
+        while (true) {
+            // Solicita ao usuário que insira o valor do depósito
+            System.out.print("Digite o valor do depósito (ou 0 para encerrar): ");
             double valor = scanner.nextDouble();
 
             if (valor > 0) {
-                System.out.println("Deposito realizado com sucesso!");
+                System.out.println("Depósito realizado com sucesso!");
                 System.out.println("Saldo atual: R$ " + df.format(valor));
             } else if (valor == 0) {
                 System.out.println("Encerrando o programa...");
                 break;
             } else {
-                System.out.println("Valor invalido! Digite um valor maior que zero.");
+                System.out.println("Valor inválido! Digite um valor maior que zero.");
             }
         }
 
         scanner.close();
     }
 }
+
